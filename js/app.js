4ad0f506e169046e42aaf47bd77630a34f53103c
PBrusset <47607449+PBrusset@users.noreply.github.com>
2021-01-30 22:27:14 +0000

    update

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 var express = require("express");
-var app = express();
-
 var mysql = require("mysql");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 var SQL_IPADDRESS = "127.0.0.1";
 var password = "mySQListhebest2021";
 
-var bodyParser = require("body-parser");
+var app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
@@ -17,6 +18,10 @@ var connection = mysql.createConnection({
   user: "root",
   password: password,
   database: "forum"
+});
+
+app.get("/", function (request, response) {
+  response.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.get("/viewproduct", function (request, response) {
@@ -38,3 +43,5 @@ app.get("/viewproduct", function (request, response) {
     console.log(results);
   });
 });
+
+app.listen(3000);
